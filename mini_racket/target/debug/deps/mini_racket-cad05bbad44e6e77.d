86bc4818e57c62cd2e956de730a810524b810f00
SalehAce1 <seyedmohammadghaem@gmail.com>
2021-11-12 10:15:50 -0500

    Removed unneeded files
    * Removed target folder
    * Removed .swp file

--- mini_racket/target/debug/deps/mini_racket-cad05bbad44e6e77.d
+++
@@ -1,5 +0,0 @@
-/Users/abhaypatel/FALL2021/CMSC388Z/FINAL/mini_racket/target/debug/deps/mini_racket-cad05bbad44e6e77.rmeta: src/main.rs
-
-/Users/abhaypatel/FALL2021/CMSC388Z/FINAL/mini_racket/target/debug/deps/mini_racket-cad05bbad44e6e77.d: src/main.rs
-
-src/main.rs:
--- mini_racket/target/debug/deps/mini_racket-ffa2c51dbd5314af.d
+++
@@ -1,5 +0,0 @@
-/Users/abhaypatel/FALL2021/CMSC388Z/FINAL/mini_racket/target/debug/deps/mini_racket-ffa2c51dbd5314af.rmeta: src/main.rs
-
-/Users/abhaypatel/FALL2021/CMSC388Z/FINAL/mini_racket/target/debug/deps/mini_racket-ffa2c51dbd5314af.d: src/main.rs
-
-src/main.rs:
